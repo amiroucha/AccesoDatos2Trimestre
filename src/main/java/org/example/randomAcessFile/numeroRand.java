9c3b8d7cffcdcf0e2ba314171ba951badf9e81b2
chaima <amirou.septiembre@gmail.com>
2025-01-22 16:39:20 +0100

    hacer la recursividad del fichero, algunos ejercicios de clase de ahora,me voy

--- src/main/java/org/example/randomAcessFile/numeroRand.java
+++ src/main/java/org/example/randomAcessFile/numeroRand.java
@@ -9,9 +9,11 @@ public class numeroRand {
     //pedir numero al suaurio, que va a ser el numero de veces de numeros randm que vamos a mostrar del 1 al 100
     public static void main(String[] args) {
         Scanner leer = new Scanner(System.in);
+
         try(RandomAccessFile raf = new RandomAccessFile("./Chaima.txt", "rw")){
             System.out.println("Introduce un numero");
             int numero = leer.nextInt();
+
             Random rand = new Random();
             for (int i=0; i<numero; i++){
                 int numRand = rand.nextInt(0,101);
